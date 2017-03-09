48438df6f885ee0c40f17bc1f865098e47f02ea4
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-03-09 11:17:42 -0500

    configured devise to redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+
   protected
 
   def configure_permitted_parameters
@@ -9,4 +11,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+
+
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable#, :omniauthable
 
-  has_many :rabbis, through: :appointments  
+  has_many :rabbis, through: :appointments
   has_many :appointments
 end
