eb05279023cb91ce1f0d8d0467b69f98bb1c41b3
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-03-14 12:12:50 -0400

    added update appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,11 +4,12 @@ class AppointmentsController < ApplicationController
   def new
     @appointment = User.find(params[:user_id]).appointments.build
     @rabbi = @appointment.build_rabbi
+
   end
 
   def create
-    user = User.find_by(id: params[:user_id])
-    
+    user = requested_user
+
     if params.require(:appointment).permit(:rabbi_id).blank?
       appointment = user.appointments.build(appointment_and_rabbi_attributes)
     else
@@ -26,6 +27,19 @@ class AppointmentsController < ApplicationController
     @appointment = requested_appointment
   end
 
+  def update
+    appointment = requested_appointment
+
+    if params.require(:appointment).permit(:rabbi_id).blank?
+      appointment.update(appointment_and_rabbi_attributes)
+    else
+      appointment.update(appointment_attributes)
+    end
+
+    redirect_to user_appointment_path(appointment.user, appointment)
+  end
+
+
   private
 
   def appointment_attributes
@@ -36,10 +50,12 @@ class AppointmentsController < ApplicationController
     params.require(:appointment).permit(:rabbi_id, :service_id, :time, :date, rabbi_attributes: [:name, :age, :years_of_experience, :branch_of_judaism, :charisma_level])
   end
 
-
-
   def requested_appointment
     Appointment.find_by(id: params[:id])
+  end
+
+  def requested_user
+    User.find_by(id: params[:user_id])
   end
 
 end
