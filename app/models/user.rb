f7f1518d403b076f5f4ce15fa82a80caf3b83ced
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-03-28 12:23:10 -0400

    added omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable#, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
 
   has_many :rabbis, through: :appointments
   has_many :appointments
   enum role: [:normal, :admin]
-  validates :name, :age,  :bothersome_level, :wallet, presence: true
+  #validates :name, :age,  :bothersome_level, :wallet, presence: true
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+     unless user
+         user = User.create(name: data["name"],
+           email: data["email"],
+            password: Devise.friendly_token[0,20]
+         )
+    end
+    user
+end
 end
---
+++ todo.md
@@ -0,0 +1,5 @@
+1. create admin index of all users.
+2. rabbi resourse with scope of how many clients and appointments.
+3. omniauth
+4. user can delete appointment
+5. issue with admin link with future appointments needs to be changed to my appointments. new link for that specifc users appointments in url.
