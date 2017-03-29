f9c9b6f629f87ae985b650bc73362422153082f8
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-03-29 12:02:17 -0400

    changed omniauth logic and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,13 @@ class User < ApplicationRecord
   enum role: [:normal, :admin]
   #validates :name, :age,  :bothersome_level, :wallet, presence: true
 
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(:email => data["email"]).first
-
-     unless user
-         user = User.create(name: data["name"],
-           email: data["email"],
-            password: Devise.friendly_token[0,20]
-         )
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      user.provider = auth.provider
     end
-    user
-end
+  end
+
 end
--- todo.md
+++ todo.md
@@ -1,5 +1,8 @@
 1. create admin index of all users.
 2. rabbi resourse with scope of how many clients and appointments.
-3. omniauth
+3. omniauth. needs gem to hide app secret flaigo chagee name from omniauth2 to google provider hash
 4. user can delete appointment
 5. issue with admin link with future appointments needs to be changed to my appointments. new link for that specifc users appointments in url.
+
+6. form select rabbi but no service returns form with filled in new rabbi. New rabbbi needs to be a form for tag.
+7
