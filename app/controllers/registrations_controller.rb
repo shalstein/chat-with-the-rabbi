32c2c138240c0112e2fd369aa639af31b0cbeff2
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-03-30 10:47:46 -0400

    users with provider can't update profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource(resource, params)
-    if current_user.provider == "facebook"
+    if current_user.provider == "google"
       params.delete("current_password")
       resource.update_without_password(params)
     else
