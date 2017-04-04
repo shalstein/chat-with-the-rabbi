80c6bdde49c71bcbd0a6829c55ef6d26076c4d7d
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-04-04 12:11:24 -0400

    resolved omniauth issue and removed user folder

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   validates :wallet, numericality: { only_integer: true}
 
   def self.from_omniauth(auth)
-    where(email: auth.info.email.first).first_or_create do |user|
+    where(email: auth.info.email).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
