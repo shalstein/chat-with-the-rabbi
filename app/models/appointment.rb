935b8582ce44054d4270c63735290d3e03507984
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-04-10 16:35:57 -0400

    appointment total price  is according to chrisma level

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -11,7 +11,6 @@ class Appointment < ApplicationRecord
 
 
 
-
   scope :future_appointments, -> (user_id) {where("user_id = ? AND time_and_date > ?",user_id, Time.current )}
 
   def rabbi_attributes=(attributes)
@@ -20,25 +19,46 @@ class Appointment < ApplicationRecord
 
 
   def charge
-    user.update(wallet: user.wallet - service.fee) if enough_money?
+    user.update(wallet: user.wallet - (service.fee + service.fee / 100 * charisma_percentage_fee)) if enough_money?
   end
 
   private
+
+  def charisma_percentage_fee
+    case rabbi.charisma_level
+      when "5"
+        25
+      when "4"
+        20
+      when "3"
+        15
+      when "2"
+        10
+      when "1"
+        5
+      else
+        0
+    end
+  end
+
+
+
+
 
   def enough_money?
     user.wallet >= service.fee
   end
 
   def does_not_conflict_other_appointments
-    errors.add(:time_and_date, "already taken by another appointment") if rabbi.appointments.exists?(time_and_date: (time_and_date - 15.minutes..time_and_date + 1.hours))
+    errors.add(:time_and_date, "already taken by another appointment") if time_and_date && rabbi.appointments.exists?(time_and_date: (time_and_date - 15.minutes..time_and_date + 1.hours))
   end
 
   def during_regular_hours
-    errors.add(:time_and_date, "not during regular hours") unless time_and_date.strftime("%H%M").to_i >= 830 && time_and_date.strftime("%H%M").to_i <= 1830
+    errors.add(:time_and_date, "not during regular hours") unless time_and_date && time_and_date.strftime("%H%M").to_i >= 830 && time_and_date.strftime("%H%M").to_i <= 1830
   end
 
   def not_on_saturday
-    errors.add(:time_and_date, "can not be on saturday") if time_and_date.saturday?
+    errors.add(:time_and_date, "can not be on saturday") if time_and_date && time_and_date.saturday?
   end
 
 
