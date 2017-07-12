178fef4bf96bf32fc4c81dfde169e64c8aa97345
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-07-12 10:54:16 -0400

    fixed issue with admin clicking on user more than once

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -8,17 +8,18 @@ $(function() {
     var source = $('#user-template').html()
     var template = Handlebars.compile(source)
     var html = template(user)
+
     $(`ul a[data-id='${user.id}']`).after(html)
   }//addUserInfo function
 
   function usersHtml(users) {
     var users_string = ""
     users.forEach(function(user) {
-      users_string += ` <ul> <h3> <a href='#' class='user' data-id='${user.id}'>  ${user.name} </a> </h3> </ul> `
+      users_string += ` <ul> <h3> <a href='#' class='user' data-id='${user.id}'>  ${user.name} </a> </h3> </ul>`
     }) //forEach and function
 
     $('article').after(users_string)
-    $('.user').on('click', function(event) {
+    $('.user').one('click', function(event) {
       event.preventDefault()
       $.getJSON(`/users/${this.dataset.id}`)
       .done(function(user) {
@@ -32,7 +33,7 @@ $(function() {
   }//usersHtml
 
   function attachListners() {
-    $('#users').on('click', function(event) {
+    $('#users').one('click', function(event) {
       event.preventDefault()
       $.get("/users")
         .done(function(users) {
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,8 @@ rab2 = Rabbi.create(first_name: "Olivia", last_name: "Oliver"   , branch_of_juda
 
 user1 = User.create(name: "tyler albertson", email: "tyeler@example.com",        password: "password")
 
+admin = User.create(name: "admin", email: "admin@example.com", password: "password", role: 1)
+
 user4 = User.create(name: "Rachel Toads", email: "rachel@example.com",    password: "password")
 
 user2 = User.create(name: "joe Toads", email: "joe@example.com",        password: "password")
@@ -31,14 +33,14 @@ ser6 = Service.create(name: "Coffee at Starbucks", fee: 250)
 
 
 
-Appointment.create(rabbi_id: rab2.id, user_id: user2.id, service_id: ser1.id, time_and_date: "2016-09-03 04:30")
+Appointment.create(rabbi_id: rab2.id, user_id: user2.id, service_id: ser1.id, time_and_date: "2018-09-03 04:30")
 
-Appointment.create(rabbi_id: rab2.id, user_id: user1.id, service_id: ser2.id, time_and_date: "2017-05-01 07:15")
-
-
-Appointment.create(rabbi_id: rab1.id, user_id: user1.id, service_id: ser2.id, time_and_date: "2017-07-02 02:45")
-
-Appointment.create(rabbi_id: rab2.id, user_id: user2.id, service_id: ser2.id, time_and_date: "2017-05-01 07:15")
+Appointment.create(rabbi_id: rab2.id, user_id: user1.id, service_id: ser2.id, time_and_date: "2018-05-01 07:15")
 
 
-Appointment.create(rabbi_id: rab1.id, user_id: user2.id, service_id: ser2.id, time_and_date: "2017-07-02 02:45")
+Appointment.create(rabbi_id: rab1.id, user_id: user1.id, service_id: ser2.id, time_and_date: "2018-07-02 02:45")
+
+Appointment.create(rabbi_id: rab2.id, user_id: user2.id, service_id: ser2.id, time_and_date: "2018-05-01 07:15")
+
+
+Appointment.create(rabbi_id: rab1.id, user_id: user2.id, service_id: ser2.id, time_and_date: "2018-07-02 02:45")
