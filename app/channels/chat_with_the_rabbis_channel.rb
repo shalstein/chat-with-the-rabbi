6b092441a19aec6632dfa99285091a64ec05640f
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-12-30 23:19:03 -0500

    added stop streaming when user disconnects

--- app/channels/chat_with_the_rabbis_channel.rb
+++ app/channels/chat_with_the_rabbis_channel.rb
@@ -1,17 +1,11 @@
 class ChatWithTheRabbisChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
     stream_from 'ChatWithTheRabbis'
-  #  stream_from 'Chatneat'
-
-
-
   end
 
 
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
     stop_all_streams
   end
 
