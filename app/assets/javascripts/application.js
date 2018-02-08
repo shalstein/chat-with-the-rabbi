be444361f9251d51d9792543bc698aa33f3a0aef
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2018-02-07 21:53:25 -0500

    precompiled assests

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,12 +9,11 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
-
 //= require handlebars-v4.0.10
-//= require jquery-ui/widgets/draggable
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery-ui/widgets/draggable
+
 //= require_tree .
